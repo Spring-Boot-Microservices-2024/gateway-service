9af273ea49b900d5f7c44cd20a71b93656c42f6b
Yevheniia Matsevko <mazevkozhenia@gmail.com>
2024-10-29 19:26:37 +0200

    configured gateway with routing and security filter

---
+++ src/main/java/com/example/gatewayservice/AuthFilter.java
@@ -0,0 +1,37 @@
+package com.example.gatewayservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthFilter implements GlobalFilter, Ordered {
+
+    @Value("${auth.header}")
+    private String authHeader;
+
+    @Value("${auth.key}")
+    private String authKey;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String apiKey = exchange.getRequest().getHeaders().getFirst(authHeader);
+
+        if (authKey.equals(apiKey)) {
+            return chain.filter(exchange);  // Proceed if API key is valid
+        }
+
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);  // Return 401 if API key is missing or invalid
+        return exchange.getResponse().setComplete();
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;  // Ensure this filter has a high priority
+    }
+}
